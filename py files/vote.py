28c40de246273becc444fbe676a185b5f1713b10
koosha lahouti <klahouti81@gmail.com>
2022-09-12 14:04:08 +0430

    last commit

---
+++ py files/vote.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+#connect to database
+conn = sqlite3.connect('vote.db')
+
+#create a cursor
+c = conn.cursor()
+
+def create_table():
+        """create a table"""
+        c.execute('''CREATE TABLE IF NOT EXISTS vote
+                (content text NOT NULL)''')
+
+
+def insert_table_vote(content):
+        try:
+                """insert into vote table"""
+                c.execute(f"INSERT INTO vote VALUES ('{content}')")
+                conn.commit()
+        except Exception as e:
+                print(e)
+
+def votes():
+        c.execute(f"SELECT * FROM vote")
+        items = c.fetchall()
+        return items[0][0]
+
+print(votes())
